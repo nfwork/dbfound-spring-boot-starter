6afb3c41ca5ee16bf23100d82c65d66887fc7af3
huangjiong <huangjiong@gomo.com>
2023-08-19 10:52:01 +0800

    优化文件上传

---
+++ src/main/java/com/github/nfwork/dbfound/starter/fileupload/SpringFilePart.java
@@ -0,0 +1,42 @@
+package com.github.nfwork.dbfound.starter.fileupload;
+
+import com.nfwork.dbfound.web.file.FilePart;
+import com.nfwork.dbfound.web.file.FileSizeCalculator;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SpringFilePart implements FilePart {
+
+    MultipartFile multipartFile;
+
+    public SpringFilePart(MultipartFile multipartFile){
+        this.multipartFile = multipartFile;
+    }
+
+    @Override
+    public String getName() {
+        return multipartFile.getOriginalFilename();
+    }
+
+    @Override
+    public String getContentType() {
+        return multipartFile.getContentType();
+    }
+
+    @Override
+    public InputStream getContent() throws IOException {
+        return multipartFile.getInputStream();
+    }
+
+    @Override
+    public String getSize() {
+        return FileSizeCalculator.getFileSize(getLength());
+    }
+
+    @Override
+    public long getLength() {
+        return multipartFile.getSize();
+    }
+}
