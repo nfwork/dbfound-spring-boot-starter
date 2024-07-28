18b3f8aacdbdc3d075bf0dc1743f7c8ad8be4444
huangjiong <huangjiong@gomo.com>
2024-07-28 12:22:14 +0800

    优化sql异常处理

--- src/main/java/com/github/nfwork/dbfound/starter/exception/DBFoundExceptionHandleImpl.java
+++ src/main/java/com/github/nfwork/dbfound/starter/exception/DBFoundExceptionHandleImpl.java
@@ -2,6 +2,7 @@ package com.github.nfwork.dbfound.starter.exception;
 
 import com.nfwork.dbfound.dto.ResponseObject;
 import com.nfwork.dbfound.exception.CollisionException;
+import com.nfwork.dbfound.util.DataUtil;
 import com.nfwork.dbfound.util.LogUtil;
 
 import javax.servlet.http.HttpServletRequest;
@@ -13,33 +14,29 @@ public class DBFoundExceptionHandleImpl implements DBFoundExceptionHandle {
 	@Override
 	public ResponseObject handle(Exception exception, HttpServletRequest request, HttpServletResponse response) {
 		ResponseObject ro = new ResponseObject();
-		try {
-			if (exception instanceof CollisionException){
-				response.setStatus(403);
-			}else{
-				response.setStatus(500);
-			}
-			String em = exception.getMessage();
-			String code = null;
-			if (exception instanceof CollisionException) {
-				code = ((CollisionException) exception).getCode();
-				LogUtil.info(exception.getClass().getName() + ": " + em);
-			}else {
-				String message = "Unexpected exception: "+exception.getClass().getName()+" caused, when request url: "+request.getRequestURI();
-				LogUtil.error(message, exception);
-				if(exception.getCause() instanceof SQLException){
-					em = exception.getCause().getMessage();
-				}
-				em = message + ", message: " + em;
-			}
-
-			ro.setMessage(em);
-			ro.setSuccess(false);
-			ro.setCode(code);
-		} catch (Exception e) {
-			LogUtil.error(e.getMessage(), e);
+		if (exception instanceof CollisionException){
+			response.setStatus(403);
+		}else{
+			response.setStatus(500);
 		}
-
+		String em = exception.getMessage();
+		String code = null;
+		if (exception instanceof CollisionException) {
+			code = ((CollisionException) exception).getCode();
+			LogUtil.info(exception.getClass().getName() + ": " + em);
+		}else {
+			String message = "an exception: "+exception.getClass().getName()+" caused, when request url: "+request.getRequestURI();
+			LogUtil.error(message, exception);
+			if(exception.getCause() instanceof SQLException){
+				em = exception.getCause().getMessage();
+			}
+		}
+		if(DataUtil.isNull(em)){
+			em = exception.getClass().getName();
+		}
+		ro.setMessage(em);
+		ro.setSuccess(false);
+		ro.setCode(code);
 		return ro;
 	}
 }
